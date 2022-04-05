2d9cd4f5c39b612ad61a83362155794e4a0d2ad5
simarpreetsinghkalra <simarpreetsinghkalra@gmail.com>
2022-04-05 13:03:37 +0530

    fix

--- operations.js
+++ operations.js
@@ -1,20 +1,25 @@
 const fs = require("fs");
 const mongoose = require("mongoose");
+const { start } = require("repl");
 
-const mongooseHelper = require("./mongoose-helper.js");
+const start = async () => {
+  const mongooseHelper = require("./mongoose-helper.js");
 
-await mongoose.connect(
-  `mongodb+srv://ewaservices:${encodeURIComponent(
-    "ewaservices2022!@#"
-  )}@cluster0.zerhe.mongodb.net/quickbook-res?retryWrites=true&w=majority`
-);
+  await mongoose.connect(
+    `mongodb+srv://ewaservices:${encodeURIComponent(
+      "ewaservices2022!@#"
+    )}@cluster0.zerhe.mongodb.net/quickbook-res?retryWrites=true&w=majority`
+  );
 
-const savedData = JSON.parse(fs.readFileSync("./data/customers-res.json"));
+  const savedData = JSON.parse(fs.readFileSync("./data/customers-res.json"));
 
-for (let i = 0; i < savedData.length; i++) {
-  const customer = new mongooseHelper.Customer({
-    displayName: savedData[i]["DisplayName"],
-    id: savedData[i]["Id"],
-  });
-  await customer.save();
-}
+  for (let i = 0; i < savedData.length; i++) {
+    const customer = new mongooseHelper.Customer({
+      displayName: savedData[i]["DisplayName"],
+      id: savedData[i]["Id"],
+    });
+    await customer.save();
+  }
+};
+
+start();
