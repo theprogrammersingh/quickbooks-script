d076847c74c2341ef5d368a386cf98754ae2c695
simarpreetsinghkalra <simarpreetsinghkalra@gmail.com>
2022-04-05 13:20:40 +0530

    fixed error

--- operations.js
+++ operations.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import mongoose from "mongoose";
-import mongooseHelper from "mongoose-helper.js";
+import mongooseHelper from "./mongoose-helper.js";
 
 const start = async () => {
   await mongoose.connect(
