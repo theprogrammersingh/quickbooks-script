a4ca592a51ebe4d5f8ef51e7b7ae9a5fa9d265f9
sharanbrar <simarpreetsinghkalra@gmail.com>
2022-05-03 08:15:56 +0530

    date year change to 2022

--- index.js
+++ index.js
@@ -124,8 +124,8 @@ const createSAInvoice = async (customerID, amount = 1000) => {
             },
           },
         ],
-        DueDate: "2020-01-31",
-        TxnDate: "2020-01-25",
+        DueDate: "2022-01-31",
+        TxnDate: "2022-01-25",
       },
       function (err, invoice) {
         if (err) {
@@ -160,8 +160,8 @@ const createFeeInvoice = (customerID, amount = 100) => {
             },
           },
         ],
-        DueDate: "2020-01-31",
-        TxnDate: "2020-01-25",
+        DueDate: "2022-01-31",
+        TxnDate: "2022-01-25",
       },
       function (err, invoice) {
         if (err) {
